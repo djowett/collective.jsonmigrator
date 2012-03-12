010a845f7d5408b5f320bdce00bc8c0640d57f40
Daniel Jowett <djowett@rcseng.ac.uk>
2012-03-12 16:45:32 +0000

    More logging when things fail. Encode filename from unicode to utf-8.

--- collective/jsonmigrator/datafields.py
+++ collective/jsonmigrator/datafields.py
@@ -5,6 +5,7 @@ from collective.transmogrifier.interfaces import ISectionBlueprint
 from collective.transmogrifier.interfaces import ISection
 from Products.Archetypes.interfaces import IBaseObject
 
+import logging
 
 class DataFields(object):
     """
@@ -19,11 +20,13 @@ class DataFields(object):
         self.datafield_prefix = options.get('datafield-prefix', '_datafield_')
         self.root_path_length = len(self.context.getPhysicalPath())
 
+
     def __iter__(self):
         for item in self.previous:
 
             # not enough info
             if '_path' not in item:
+                logging.getLogger("jsonmigrator.datafields").info("Path not in item")
                 yield item
                 continue
 
@@ -32,6 +35,8 @@ class DataFields(object):
 
             # path doesn't exist
             if obj is None:
+                logging.getLogger("jsonmigrator.datafields").info("Path doesn't exist in %s %s" %
+                                                                  (self.context, item['_path']))
                 yield item
                 continue
 
@@ -40,6 +45,9 @@ class DataFields(object):
             if path.startswith('/'):
                 path = path[1:]
             if '/'.join(obj.getPhysicalPath()[self.root_path_length:]) != path:
+                logging.getLogger("jsonmigrator.datafields").info("Wrong object got thru acquisition \nobject: %s, \nitem path: %s" %
+                                                                  ('/'.join(obj.getPhysicalPath()[self.root_path_length:]),
+                                                                   path))
                 yield item
                 continue
 
@@ -50,6 +58,7 @@ class DataFields(object):
                         continue
 
                     fieldname = key[len(self.datafield_prefix):]
+                    #logging.getLogger("jsonmigrator.datafields").info("Decoding field %s" % (fieldname))
                     field = obj.getField(fieldname)
                     if field is None:
                         continue
@@ -59,7 +68,10 @@ class DataFields(object):
                     old_value = field.get(obj).data
                     if value != old_value:
                         field.set(obj, value)
-                        obj.setFilename(item[key]['filename'])
+                        filename = item[key]['filename']
+                        if isinstance( filename, unicode):
+                            filename = filename.encode('utf-8')
+                        obj.setFilename(filename)
                         obj.setContentType(item[key]['content_type'])
 
             yield item
